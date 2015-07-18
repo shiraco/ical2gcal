72e9b6b7a7d68a81db8e830d3562c0bac46bdcc9
Koji Shiraishi <shiraco@gmail.com>
2015-07-18 14:01:25 +0900

    Apply batch delete

--- ical2gcal.rb
+++ ical2gcal.rb
@@ -9,7 +9,6 @@ SECRET_KEY_PASSWORD = ENV['SECRET_KEY_PASSWORD']
 CALENDAR_ID = ENV['CALENDAR_ID']
 SERVICE_ACCOUNT_EMAIL = ENV['SERVICE_ACCOUNT_EMAIL']
 APPLICATION_NAME = ENV['APPLICATION_NAME']
-MAX_BATCH_SIZE = 1000
 TERRA_TERM = 3
 
 # Initialize the API
@@ -31,32 +30,42 @@ calendar_api = client.discovered_api('calendar', 'v3')
 # カレンダー削除
 today = Date.today
 s = Date.new(today.year, today.month, 1)
-s = (s << 5)
+s = (s << 5) # TODO remove
 e = (s >> TERRA_TERM) - 1
 
 time_min = Time.local(s.year, s.month, s.day, 0, 0, 0).iso8601
 time_max = Time.local(e.year, e.month, e.day, 23, 59, 59).iso8601
 
-old_events = client.execute(:api_method => calendar_api.events.list,
-                            :parameters =>  {'calendarId' => CALENDAR_ID,
-                                             'timeMin' => time_min,
-                                             'timeMax' => time_max})
+old_events_per_page = client.execute(:api_method => calendar_api.events.list,
+                                     :parameters =>  {'calendarId' => CALENDAR_ID,
+                                                      'timeMin' => time_min,
+                                                      'timeMax' => time_max})
+
+old_events = []
 
 while true
-  old_events.data.items.each do |event|
-    client.execute(:api_method => calendar_api.events.delete,
-                   :parameters => {'calendarId' => CALENDAR_ID,
-                                   'eventId' => event["id"]})
-  end
-  if !(page_token = old_events.data.next_page_token)
+  old_events.concat(old_events_per_page.data.items)
+
+  if !(page_token = old_events_per_page.data.next_page_token)
     break
   end
-  old_events = client.execute(:api_method => calendar_api.events.list,
-                              :parameters =>  {'calendarId' => CALENDAR_ID,
-                                               'timeMin' => time_min,
-                                               'timeMax' => time_max,
-                                               'pageToken' => page_token})
+  old_events_per_page = client.execute(:api_method => calendar_api.events.list,
+                                       :parameters =>  {'calendarId' => CALENDAR_ID,
+                                                        'timeMin' => time_min,
+                                                        'timeMax' => time_max,
+                                                        'pageToken' => page_token})
 end
+
+batch_delete = Google::APIClient::BatchRequest.new
+
+# 出力
+old_events.each do |event|
+  batch_delete.add(:api_method => calendar_api.events.delete,
+                   :parameters => {'calendarId' => CALENDAR_ID,
+                                   'eventId' => event.id})
+end
+
+client.execute(batch_delete)
 
 # ical読み込み
 ical = Icalendar.parse(File.read('calendar.ics')).first
@@ -73,16 +82,15 @@ new_events = new_events.map do |event|
     url: event.url.to_s
   }
 end
-puts "events: #{new_events[0]}"
 
 # カレンダー登録
-batch = Google::APIClient::BatchRequest.new
+batch_insert = Google::APIClient::BatchRequest.new
 
 new_events.each do |event|
-  batch.add(:api_method => calendar_api.events.insert,
-            :parameters => {'calendarId' => CALENDAR_ID},
-            :body => JSON.dump(event),
-            :headers => {'Content-Type' => 'application/json'})
+  batch_insert.add(:api_method => calendar_api.events.insert,
+                   :parameters => {'calendarId' => CALENDAR_ID},
+                   :body => JSON.dump(event),
+                   :headers => {'Content-Type' => 'application/json'})
 end
 
-client.execute(batch)
+client.execute(batch_insert)
